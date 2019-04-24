cfcd37ea80988cb18ee155b0ce9ec38681293ba9
Jelle Pals <jellepals@Jelles-MacBook-Pro.local>
2019-04-24 11:56:34 +0200

    Created the app

--- calculatorApp/src/app/home/home.page.ts
+++ calculatorApp/src/app/home/home.page.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  userIsCurrentlyTyping: boolean = false;
+  displayValue: string; 
+  currentOperand: string;
+  errorMessage: string;
+  get currentValue(){
+    return parseFloat(this.displayValue)
+  };
+  accumulator: number;
+
+  constructor(){
+    this.allClear();
+  }
+
+  touchDigit(event){
+    let digit = event.srcElement.innerText;
+    if(!this.userIsCurrentlyTyping){
+      this.displayValue = digit; 
+      this.userIsCurrentlyTyping = true;
+    } else {
+      this.displayValue = this.displayValue + digit; 
+    }
+  }
+
+  touchBinOp(event){
+    this.currentOperand = event.srcElement.innerText;
+    this.accumulator = this.currentValue;
+    this.userIsCurrentlyTyping = false; 
+  }
+
+  touchUnOp(event){
+    this.currentOperand = event.srcElement.innerText;
+    switch(this.currentOperand){
+      case '%': this.displayValue = (this.currentValue/100).toString();  break;
+      case '+/-': this.displayValue = (-1*this.currentValue).toString();  break;
+      default: this.errorMessage = `Onbekende operator $(this.currentOperand)`; break;
+    }
+  }
+
+  touchEquals(){
+    switch(this.currentOperand){
+      case "-": this.displayValue = (this.accumulator - this.currentValue).toString(); break;
+      case "+": this.displayValue = (this.accumulator + this.currentValue).toString(); break;
+      case "+/-": this.displayValue = (this.accumulator / this.currentValue).toString(); break;
+      case "*": this.displayValue = (this.accumulator * this.currentValue).toString(); break;
+      default: this.errorMessage = `Onbekende operator $(this.currentOperand)`; break;
+    }
+    this.userIsCurrentlyTyping = false;
+  }
+
+  allClear(){
+    this.displayValue = "0"; 
+    this.userIsCurrentlyTyping = false;
+    this.errorMessage = '';
+  }
 
 }
